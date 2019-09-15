1e817692161ead35e27b4b7ccabaa30f585140d9
Angad Bir Singh Sandhu <angadsandhu@Angads-MacBook-Pro.local>
2019-09-15 19:34:32 +0530

    [0007] enhancements to server side code & added action buttons to post list

--- server/express.js
+++ server/express.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res)=>{
-  console.log('First Middleware');
-  next();
-});
-
-app.use((req,res)=>{
-  res.send('Response Comming from Server : Hi there !!');
+app.use('/posts',(req,res)=>{
+  const posts  = [{
+    id: 'a1f45',
+    title: 'First Post',
+    description: 'I come from the server'
+  },{
+    id:'89ade45gb',
+    title: 'Second Post',
+    description: 'I too come from the server'
+  }];
+  res.json({
+    message: 'Succesfully fetched Posts',
+    posts: posts
+  });
+  console.log(res.json({
+    message: 'Succesfully fetched Posts',
+    posts: posts
+  }));
 });
 
 module.exports = app;
