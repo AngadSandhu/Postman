75b152ef9fede851e75ffc4a698ecf9c4a436e43
Angad Bir Singh Sandhu <angadsandhu@Angads-MacBook-Pro.local>
2019-09-15 19:58:40 +0530

    [0008] Added CORS Headers and middleware to handle cors req in express js file

--- server/express.js
+++ server/express.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+
+// Middleware for handling CORS
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","*");  // allows request from any domain
+  res.setHeader("Access-Control-Allow-Header", "Origin ,X-Requested-With, Content-Type, Accept");  // allows all these specified headers in the request. Any other header not specified here shall not be allowed to access
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE,OPTIONS");
+  next();
+});
+
 app.use('/posts',(req,res)=>{
   const posts  = [{
     id: 'a1f45',
